5381d2967d99bce5fc427532888154668c57fd59
Bharath Kumar <bharathabdeviellers@gmail.comgit config --global user.email bharathabdeviellers@gmail.com>
2022-08-25 12:38:18 +0530

    added day-2

---
+++ zappos/src/Components/Footer.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import "../styles/Footer.css"
+
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className='foot1'>
+            <h2>Explore Zappos</h2>
+            <p>Brands</p>
+            <p>Clothing</p>
+            <p>The Style Room</p>
+            <p>Eyewear</p>
+            <p>New Arrivals</p>
+            <p>Running</p>
+            <p>Jackets</p>
+            <p>Shoes</p>
+            <p>Watches</p>
+
+        </div>
+        <div className='foot2'>
+            <h2>Customer Service</h2>
+            <p>FAQs</p>
+            <p>Contact Info</p>
+            <p>Shipping And Returns Policy</p>
+            <p>About Proposition 65</p>
+
+        </div>
+        <div className='foot3'>
+            <h2>About Zappos</h2>
+            <p>About</p>
+            <p>Zappos ONE</p>
+            <p>Zappos for Good</p>
+            <p>Get the Zappos Mobile App</p>
+            <p>Amazon Prime Benefits</p>
+
+        </div>
+        <div className='foot4'>
+     
+            <h2>Connect With Us</h2>
+
+            
+            <img src="https://imgs.search.brave.com/deeHohLOdGSkR_3O9WPzGprlmSUagO4TRGz5MtBKtz0/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9mL2ZmL0Zh/Y2Vib29rX2xvZ29f/MzZ4MzYuc3ZnLzEy/MDBweC1GYWNlYm9v/a19sb2dvXzM2eDM2/LnN2Zy5wbmc" alt="f" />
+            <img src="https://imgs.search.brave.com/HSqZIViVT05nuvKYi1zxI4wa9U4S0cYVgXJBDNUjowc/rs:fit:1200:1200:1/g:ce/aHR0cDovLzEwMDBs/b2dvcy5uZXQvd3At/Y29udGVudC91cGxv/YWRzLzIwMTcvMDYv/VHdpdHRlci1Mb2dv/LnBuZw" alt="" />
+            <img src="https://imgs.search.brave.com/IDrvaNJN6gpiYix9Ok-oXDYw5irsH5_3hRjUdIe-fZ4/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly93d3cu/dGVjaG5vYmV6ei5j/b20vZmlsZXMvdXBs/b2Fkcy8yMDE1LzAy/L2luc3RhZ3JhbS0x/NTgxMjY2XzE5MjAu/anBn" alt="" />
+            <img src="https://imgs.search.brave.com/7ulQhDe-3Xal-c1RuURahETWpqbDpRRV1HkcyipG0Mw/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly8xMDAw/bG9nb3MubmV0L3dw/LWNvbnRlbnQvdXBs/b2Fkcy8yMDE3LzA1/L05ldy1Zb3VUdWJl/LWxvZ28uanBn" alt="" />
+
+
+
+            </div>
+            
+        </div>
+   
+  )
+}
+
+export default Footer
